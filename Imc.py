f0607c71602f57814ada937d053bd042968cfc71
Danizinh <danimaltoc@gmai.com>
2022-03-22 00:50:49 -0300

    sixth-commit

--- Imc.py
+++ Imc.py
@@ -5,16 +5,17 @@ Weight = float(input("Enter your Weight:"))
 Height = float(input("Enter your Height:"))
 BMI = Weight / (Height * Height)
 if (BMI < 18.5):
-       print(f"{IMC:.5f} you are thin")
+       print(f"{BMI:.5f} you are thin")
 elif (BMI < 24.5):
-        print(f"{IMC:.5f} you are normal")
+        print(f"{BMI:.5f} you are normal")
 elif (BMI < 29.9):
-    print(f"{IMC:.5} you are overweight I")
+    print(f"{BMI:.5} you are overweight I")
 elif (BMI < 34.9):
-    print(f"{IMC:.5} you areobese I")
+    print(f"{BMI:.5} you areobese I")
 elif (BMI < 39.9):
-    print(f"{IMC:.5} you areobese II")
+    print(f"{BMI:.5} you areobese II")
 elif(BMI > 40):
     print("YOU ARE morbidly obese III")
-
+print("******************************************************")
 print("END TO CALCULATOR")
+print("******************************************************")
